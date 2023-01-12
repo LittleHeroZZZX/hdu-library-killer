4fb91d0abd20c7aa86290a087d81d5b59c463e71
littleherozzzx <zhou.xin2022.code@outlook.com>
2023-01-12 13:17:33 +0800

    完成ConfigParser类。

---
+++ config/config.py
@@ -0,0 +1,58 @@
+'''
+Author: littleherozzzx zhou.xin2022.code@outlook.com
+Date: 2023-01-12 12:49:30
+LastEditTime: 2023-01-12 13:11:00
+Software: VSCode
+'''
+
+import yaml
+import os
+
+class ConfigParser():
+    def __init__(self, configFile):
+        self.configFile = configFile
+        self.config = None
+        self.template = """
+data:
+  query_data:
+    beginTime: null
+    duration: null
+    num: '1'
+    space_category[category_id]: '591'
+    space_category[content_id]: '3'
+seat_list: []
+session:
+  headers:
+    User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101
+      Firefox/101.0
+  params:
+    LAB_JSON: '1'
+  trust_env: false
+  urls:
+    book_seat: https://hdu.huitu.zhishulib.com/Seat/Index/bookSeats
+    login: https://hdu.huitu.zhishulib.com/User/Index/login
+    query_seats: https://hdu.huitu.zhishulib.com/Seat/Index/searchSeats
+  verify: false
+settings:
+  interval: 3
+  max_try_times: 50
+user_info:
+  login_name: 
+  org_id: '104'
+  password: 
+        """
+    
+    def createConfig(self, config):
+        with open(self.configFile, 'w+') as f:
+            self.config = yaml.load(self.template, Loader=yaml.FullLoader)
+            yaml.dump(self.config, f)
+    
+    def parseConfig(self):
+        with open(self.configFile, 'r') as f:
+            self.config = yaml.load(f, Loader=yaml.FullLoader)
+        return self.config
+
+
+if __name__ == "__main__":
+    config = ConfigParser("config.yaml")
+    config.createConfig(config)
