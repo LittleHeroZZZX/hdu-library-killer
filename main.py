45c89e3e658d5c29138eefedda12058b7a0544e7
littleherozzzx <zhou.xin2022.code@outlook.com>
2023-01-15 23:33:35 +0800

    完成菜单，修改不满足驼峰命名的一个函数。

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 '''
 Author: littleherozzzx zhou.xin2022.code@outlook.com
 Date: 2023-01-12 16:38:00
-LastEditTime: 2023-01-12 18:57:58
+LastEditTime: 2023-01-15 23:32:48
 Software: VSCode
 '''
 import os
@@ -16,6 +16,7 @@ class UserInterface:
     def __init__(self):
         self.configFile = "./config/config.yaml"
         self.killer = Killer()
+        self.funcs = [self.changePlan, self.changeTime, self.startNow, self.startAt, self.help, self.exit]
     
     
     def init(self):
@@ -34,7 +35,7 @@ class UserInterface:
             print(f"配置文件解析成功。")
             sleep(1)
 
-    def set_user_info(self):
+    def setUserInfo(self):
         userInfo = {}
         userInfo["login_name"] = input("请输入学号：")
         userInfo["password"] = pwinput("请输入密码：")
@@ -50,13 +51,39 @@ class UserInterface:
                     flag = True
                 else:
                     print("配置文件中账号密码错误，请重新输入")
-                    self.set_user_info()
+                    self.setUserInfo()
             else:
-                self.set_user_info()
-                
+                self.setUserInfo()
+    
+    def showMenu(self):
+        print("1. 添加/删除待选座位方案")
+        print("2. 批量修改方案中预约时间")
+        print("3. 立即开始抢座")
+        print("4. 定时抢座")
+        print("5. 使用帮助")
+        print("6. 退出")
+    
+    def changePlan(self):
+        pass
+    
+    def changeTime(self):
+        pass
+    
+    def startNow(self):
+        pass
+
+    def startAt(self):
+        pass
+    
+    def help(self):
+        pass
+    
+    def exit(self):
+        exit(0)
 if __name__ == "__main__":
     ui = UserInterface()
     ui.init()
     ui.login()
+    ui.showMenu()
  
         
